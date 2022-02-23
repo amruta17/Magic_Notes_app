7be26dca38200d60d248d369ca58d08336f862f0
amruta <pa17april@gmail.com>
2022-02-23 15:19:16 +0530

    docs:delete done

--- app.js
+++ app.js
@@ -40,12 +40,12 @@ function shownotes(){
 
 let html="";
 notesObj.forEach(function(element, index){
-    html +=`<div class=" my-2 mx-2 card" style="width: 18rem;">
+    html +=`<div class=" my-2 mx-2 card" style="width: 18rem; background: linear-gradient(120deg, red, yellow, rgb(204, 204, 255)); color:black;">
 
-    <div class="card-body">
+    <div class="card-body" style="background: linear-gradient(120deg, red, yellow, rgb(204, 204, 255)); color:black;">
         <h5 class="card-title">Note ${index+1}</h5>
-        <p class="card-text">${element}</p>
-        <button style=" background: linear-gradient(120deg, red, yellow, rgb(204, 204, 255)); color: black "  class="btn btn-primary"> delete note</button>
+        <p class="card-text" style="background: linear-gradient(120deg, red, yellow, rgb(204, 204, 255)); color:black;">${element}</p>
+        <button  id="${index+1}" onclick="deleteNote(this.id)" "style=" background: linear-gradient(120deg, red, yellow, rgb(204, 204, 255)); color: black "  class="btn btn-primary"> delete note</button>
     </div>
 </div>`;
 
@@ -58,3 +58,57 @@ else{
     notesElm.innerHTML=`<h4>Nothing to show yet! use  above add a note section to add new notes and enjoy the app</h4>`
 }
 }
+
+//function to delete a note 
+function deleteNote(index){
+    console.log("i am deleting",index);
+    let notes= localStorage.getItem('notes');
+     //console.log(notes);
+     if(notes=== null) {
+        
+        notesObj=[];
+        
+     }
+     else{
+        notesObj=JSON.parse(notes);
+         
+     }
+     notesObj.splice(index,1);
+     localStorage.setItem("notes", JSON.stringify(notesObj));
+     shownotes();
+        
+}
+// for serching
+let searchTxt=document.getElementById('searchtxt');
+searchTxt.addEventListener("input",function(){
+
+let inputval=searchTxt.value;
+    console.log("input event fired", inputval);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
